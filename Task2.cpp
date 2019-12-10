d790acf97658080b3dc92275783215892511a9ab
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2019-12-10 21:28:57 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -16,21 +16,25 @@ int main()
 	createRuleTest();
 	encodingTests();
 	decodingTests();
+	system("pause");
 }
 
 char* createRule(const char* source)
 {
 	// TODO reverse source here
+	return nullptr;
 }
 
 int* encoding(const char * source, const char * rule)
 {
 	// TODO encoding logic here
+	return nullptr;
 }
 
 char* decoding(const int * source, const char * rule)
 {
 	// TODO decoding logic here
+	return nullptr;
 }
 
 bool equals(const char * lhs, const char * rhs)
@@ -61,9 +65,14 @@ void encodingTests()
 
 	const char* rule = createRule(source);
 
-	const int * encode = encoding("Learn C++!", rule);
+	int * encode = encoding("Learn C++!", rule);
 
-	cout << equals(encode, new int [] {15, 22, 26, 9, 13, 32, 24, 43, 43, 33}) << endl;
+	cout << "Test for encoding " << (equals(encode, new int [10] {15, 22, 26, 9, 13, 32, 24, 43, 43, 33}) ? "Passed." : "Failed.") << endl;
+	
+	encode = encoding("Cogito, ergo sum! (Descartes)", rule);
+	
+	cout << "Test for encoding " << (equals(encode, new int [29] {24, 12, 20, 18, 7, 12, 44, 32, 22, 9, 20, 12, 32, 8, 6, 14, 33, 32, 40, 23, 22, 8, 24, 26, 9, 7, 22, 8, 41}) ? "Passed." : "Failed.") << endl;
+
 }
 
 void decodingTests()
@@ -72,7 +81,11 @@ void decodingTests()
 
 	const char* rule = createRule(source);
 
-	const char* decode = decoding(new int [] {15, 22, 26, 9, 13, 32, 24, 43, 43, 33}, rule);
+	char* decode = decoding(new int [10] {15, 22, 26, 9, 13, 32, 24, 43, 43, 33}, rule);
 
-	cout << equals(decode, "Learn C++!") << endl;
+	cout << "Test for decoding " << (equals(decode, "Learn C++!") ? "Passed." : "Failed.") << endl;
+
+	decode = decoding(new int [29] {24, 12, 20, 18, 7, 12, 44, 32, 22, 9, 20, 12, 32, 8, 6, 14, 33, 32, 40, 23, 22, 8, 24, 26, 9, 7, 22, 8, 41}, rule);
+
+	cout << "Test for decoding " << (equals(decode, "Cogito, ergo sum! (Descartes)") ? "Passed." : "Failed.") << endl;
 }
