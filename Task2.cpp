48a7c0fc96e9d2ce1ea4cf1d1c9607405607d4b9
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2019-12-11 22:55:14 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int* encoding(char const*, char const*);
 char* decoding(int const*, int, char const*);
-char* createRule(const char *);
+char* createRule(const char*);
 char* createRule(const char*, int);
 void createRuleTest();
 void encodingTests();
 void decodingTests();
-bool equals(const char *, const char *);
+bool equals(const char*, const char*);
 bool equals(const int*, int, const int*, int);
+bool isAlphabet(char);
+char toUpper(char);
+int indexOf(const char*, char);
+int getLength(const char*);
 
 int main()
 {
@@ -22,34 +26,64 @@ int main()
 
 char* createRule(const char* source)
 {
-	// TODO reverse source here
-	return nullptr;
+	int length = getLength(source);
+
+	char* rule = new char[length + 1];
+
+	//TODO
+
+	return rule;
 }
 
-int* encoding(const char * source, const char * rule)
+int* encoding(const char* source, const char* rule)
 {
-	// TODO encoding logic here
-	return nullptr;
+	int length = getLength(source);
+
+	int* encode = new int[length];
+
+	//TODO
+
+	return encode;
 }
 
-char* decoding(const int * source, int n, const char * rule)
+int indexOf(const char* source, char symbol)
 {
-	// TODO decoding logic here
-	return nullptr;
+	//TODO
+
+	return -1;
 }
 
-bool equals(const char * lhs, const char * rhs)
+bool isAlphabet(char symbol)
 {
-	// TODO
-
+	//TODO
 	return false;
+}
+
+char toUpper(char symbol)
+{
+	//TODO
+	return symbol;
+}
+
+char* decoding(const int* source, int n, const char* rule)
+{
+	char* decode = new char[n + 1];
+	//TODO
+	return decode;
+}
+
+bool equals(const char* lhs, const char* rhs)
+{
+	//TODO
+
+	return true;
 }
 
 bool equals(const int* lhs, int n, const int* rhs, int m)
 {
-	// TODO
+	//TODO
 
-	return false;
+	return true;
 }
 
 void createRuleTest()
@@ -61,12 +95,16 @@ void createRuleTest()
 	const char* expected = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
 
 	cout << "Test for creation rule " << (equals(actual, expected) ? "Passed." : "Failed.") << endl;
-	
+
+	delete[] actual;
+
 	actual = createRule(source, 5);
 
-	char* expected = "UTSRQPONMLKJIHGFEDCBAZYXWV";
+	expected = "UTSRQPONMLKJIHGFEDCBAZYXWV";
 
 	cout << "Test for creation rule " << (equals(actual, expected) ? "Passed." : "Failed.") << endl;
+
+	delete[] actual;
 }
 
 void encodingTests()
@@ -75,26 +113,37 @@ void encodingTests()
 
 	const char* rule = createRule(source);
 
-	int * encode = encoding("Learn C++!", rule);
+	int* encode = encoding("Learn C++!", rule);
 
-	cout << "Test for encoding " << (equals(encode, 10, new int [10] {15, 22, 26, 9, 13, 32, 24, 43, 43, 33}, 10) ? "Passed." : "Failed.") << endl;
-	
+	cout << "Test for encoding " << (equals(encode, 10, new int[10]{ 15, 22, 26, 9, 13, 32, 24, 43, 43, 33 }, 10) ? "Passed." : "Failed.") << endl;
+
+	delete[] encode;
+
 	encode = encoding("Cogito, ergo sum! (Descartes)", rule);
-	
-	cout << "Test for encoding " << (equals(encode, 29, new int [29] {24, 12, 20, 18, 7, 12, 44, 32, 22, 9, 20, 12, 32, 8, 6, 14, 33, 32, 40, 23, 22, 8, 24, 26, 9, 7, 22, 8, 41}, 29) ? "Passed." : "Failed.") << endl;
+
+	cout << "Test for encoding " << (equals(encode, 29, new int[29]{ 24, 12, 20, 18, 7, 12, 44, 32, 22, 9, 20, 12, 32, 8, 6, 14, 33, 32, 40, 23, 22, 8, 24, 26, 9, 7, 22, 8, 41 }, 29) ? "Passed." : "Failed.") << endl;
+
+	delete[] encode;
+
+	delete[] rule;
 
 	// new shift rule
-	
+
 	rule = createRule(source, 5);
 
 	encode = encoding("Learn C++!", rule);
 
 	cout << "Test for encoding " << (equals(encode, 10, new int[10]{ 10, 17, 21, 4, 8, 32, 19, 43, 43, 33 }, 10) ? "Passed." : "Failed.") << endl;
 
+	delete[] encode;
+
 	encode = encoding("Cogito, ergo sum! (Descartes)", rule);
 
 	cout << "Test for encoding " << (equals(encode, 29, new int[29]{ 19, 7, 15, 13, 2, 7, 44, 32, 17, 4, 15, 7, 32, 3, 1, 9, 33, 32, 40, 18, 17, 3, 19, 21, 4, 2, 17, 3, 41 }, 29) ? "Passed." : "Failed.") << endl;
 
+	delete[] encode;
+
+	delete[] rule;
 }
 
 void decodingTests()
@@ -103,31 +152,56 @@ void decodingTests()
 
 	const char* rule = createRule(source);
 
-	char* decode = decoding(new int [10] {15, 22, 26, 9, 13, 32, 24, 43, 43, 33}, 10, rule);
+	char* decode = decoding(new int[10]{ 15, 22, 26, 9, 13, 32, 24, 43, 43, 33 }, 10, rule);
 
 	cout << "Test for decoding " << (equals(decode, "LEARN C++!") ? "Passed." : "Failed.") << endl;
 
-	decode = decoding(new int [29] {24, 12, 20, 18, 7, 12, 44, 32, 22, 9, 20, 12, 32, 8, 6, 14, 33, 32, 40, 23, 22, 8, 24, 26, 9, 7, 22, 8, 41}, 29, rule);
+	delete[] decode;
+
+	decode = decoding(new int[29]{ 24, 12, 20, 18, 7, 12, 44, 32, 22, 9, 20, 12, 32, 8, 6, 14, 33, 32, 40, 23, 22, 8, 24, 26, 9, 7, 22, 8, 41 }, 29, rule);
 
 	cout << "Test for decoding " << (equals(decode, "COGITO, ERGO SUM! (DESCARTES)") ? "Passed." : "Failed.") << endl;
 
+	delete[] decode;
+
+	delete[] rule;
+
 	// new shift rule
-	
+
 	rule = createRule(source, 5);
 
 	decode = decoding(new int[10]{ 10, 17, 21, 4, 8, 32, 19, 43, 43, 33 }, 10, rule);
 
 	cout << "Test for decoding " << (equals(decode, "LEARN C++!") ? "Passed." : "Failed.") << endl;
 
+	delete[] decode;
+
 	decode = decoding(new int[29]{ 19, 7, 15, 13, 2, 7, 44, 32, 17, 4, 15, 7, 32, 3, 1, 9, 33, 32, 40, 18, 17, 3, 19, 21, 4, 2, 17, 3, 41 }, 29, rule);
 
 	cout << "Test for decoding " << (equals(decode, "COGITO, ERGO SUM! (DESCARTES)") ? "Passed." : "Failed.") << endl;
+
+	delete[] decode;
+
+	delete[] rule;
+
 }
 
 char* createRule(const char* source, int shift)
 {
-	// TODO 
-	// reverse source
-	// shift left 
-	return nullptr;
+	char* rule = createRule(source);
+
+	int length = getLength(source);
+
+	//TODO
+
+	return rule;
+}
+
+int getLength(const char* source)
+{
+	int length = 0;
+
+	//TODO
+
+	return length;
 }
